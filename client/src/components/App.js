224ba5b1933e72c564de9419974b079f7cb35678
Thanaphon Sombunkaeo <ict224bj@gmail.com>
2020-06-29 20:41:33 +0700

    [UPDATE] landing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -14,7 +14,8 @@ class App extends Component {
     render() {
         return (
             <div id="app">
-                <h1>BTS live view count</h1>
+                <img style={{marginBottom:'25px'}} alt="bts-log" width="auto" height="50%" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a7247d6a-142c-47a6-a615-f88f13936642/dbf8j7j-e5b54ac0-3107-4c18-aa16-d34345b90543.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvYTcyNDdkNmEtMTQyYy00N2E2LWE2MTUtZjg4ZjEzOTM2NjQyXC9kYmY4ajdqLWU1YjU0YWMwLTMxMDctNGMxOC1hYTE2LWQzNDM0NWI5MDU0My5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ._RcuNCRWbumCjBmc99kFMoov1ukOJjmA4EiUCFOgp_0"></img>
+                <h1 style={{ letterSpacing: '5px' }}>BTS live view count</h1>
             </div>
         );
     }
