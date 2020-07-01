30d5e5c67cd20260a9f1a01ab44ffce5dd8010c9
Thanaphon Sombunkaeo <ict224bj@gmail.com>
2020-07-02 01:55:11 +0700

    [UPDATE] google client id

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientId: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY,
