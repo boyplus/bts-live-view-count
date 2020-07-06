19005e2bac617883edb48ffe1a3cb29537d283f2
Thanaphon Sombunkaeo <ict224bj@gmail.com>
2020-07-07 02:32:28 +0700

    [UPDATE] admin page

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -22,7 +22,7 @@ module.exports = (app) => {
 
     app.get('/api/logout', (req, res) => {
         req.logout();
-        req.redirect('/');
+        res.redirect('/');
     });
 
     app.get('/api/current_user', (req, res) => {
